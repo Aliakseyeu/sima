a1399ba87ea5c9c9f963a83b05113b0b45896e4c
Siarhei Aliakseyeu <e.onizuka@mail.ru>
2019-06-25 20:25:43 +0300

    Fixes checking is admin in group info block

--- resources/views/orders/list/group-info.blade.php
+++ resources/views/orders/list/group-info.blade.php
@@ -1,6 +1,6 @@
 <div class="float-left pt-2">
     <b>Группа №{{ $group->id }}. Создана {{ $group->created_at }}</b>
-    @if(Auth::user()->isAdmin())
+    @if(Auth::id() && Auth::user()->isAdmin())
         <a href='/group/{{ $group->id }}/show/?page={{ Request::get('page') }}' title='Редактировать' ><i class="fa fa-pen"></i></a>
     @endif
     {{ $group->comment }}
