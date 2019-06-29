711f58251414ad62d3a62621a2e49a3168f7f379
Siarhei Aliakseyeu <e.onizuka@mail.ru>
2019-06-29 10:35:17 +0300

    Fixes bug with empty page number in form when updates group comment

--- resources/views/group/show.blade.php
+++ resources/views/group/show.blade.php
@@ -10,7 +10,7 @@
 <form action='/group/update' method='POST' class='form'>
     @csrf
     <input type='hidden' name='id' value='{{ $group->id }}'>
-    <input type='hidden' name='page' value='{{ Request::get('page', 1) }}'>
+    <input type='hidden' name='page' value='{{ Request::get('page') ?? 1 }}'>
     <div class="form-group">
         <label for="comment">Комментарий</label>
         <input type='text' class="form-control" name='comment' value='{{ $group->comment }}' placeholder='Комментарий'>
